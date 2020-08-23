758dcadfcb36383bd83c671038d70c919479708e
saxumcordis <victor123952@gmail.com>
2020-08-23 22:42:15 +0300

    add account personal view

--- src/Components/UserParts/Account.js
+++ src/Components/UserParts/Account.js
@@ -13,8 +13,8 @@ const Account = () => {
     });
 
     const logOut = () => {
-      logout();
-      window.location.reload();
+        logout();
+        window.location.reload();
     };
 
     if (user === 'guest')
@@ -32,10 +32,43 @@ const Account = () => {
                     <div className='account_left'>
                     </div>
                     <div className='account_right'>
-                            <div className="account_right_title">
-                                <h1>Личный кабинет</h1>
-                                <h3 onClick={() => logOut()}>Выход</h3>
+                        <div className="account_right_title">
+                            <h1>Личный кабинет</h1>
+                            <h3 onClick={() => logOut()}>Выход</h3>
+                        </div>
+                        <div className='account_right_personal'>
+                            <p>Подчёркнутые поля можно изменить. Для изменения поля кликните на него.</p>
+                            <div className='account_right_personal_edit'>
+                                <p className='account_right_personal_field'>
+                                    <span className='personal_field'>Имя:</span>
+                                    <span className='personal_field value'>SOSIK</span>
+                                </p>
+                                <form className="personal_edit_form">
+                                    <p className="">
+                                        <label>Новое имя</label>
+                                        <input required="" type="text" name="NAME" value="SOSIK"
+                                               data-place="SOSIK" className=""/>
+                                    </p>
+                                    <input type="submit" value="Сохранить"/>
+                                </form>
+                                <p className='account_right_personal_field'>
+                                    <span className='personal_field'>Фамилия:</span>
+                                    <span className='personal_field value'>SOSIK</span>
+                                </p>
+                                <p className='account_right_personal_field'>
+                                    <span className='personal_field'>Логин:</span>
+                                    <span className='personal_field'>1</span>
+                                </p>
+                                <p className='account_right_personal_field'>
+                                    <span className='personal_field'>Пароль:</span>
+                                    <span className='personal_field value'>***********</span>
+                                </p>
+                                <p className='account_right_personal_field'>
+                                    <span className='personal_field'>Дата рождения</span>
+                                    <span className='personal_field value'>1</span>
+                                </p>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
