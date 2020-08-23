aec639a4d61cf0d6a64d207317bb206023b07ef1
saxumcordis <victor123952@gmail.com>
2020-08-23 22:45:21 +0300

    add personal info to user context

--- src/Service/Contexts/UserContext.js
+++ src/Service/Contexts/UserContext.js
@@ -7,20 +7,21 @@ export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({children}) => {
     const [user, setUser] = useState(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : 'guest');
+    const [personal, setPersonal] = useState(localStorage.getItem('personal') ? JSON.parse(localStorage.getItem('personal')) : []);
     const [stageStatus, setStageStatus] = useState(0);
-    const login = useCallback((user) => setUser(user), [setUser]);
     const logout = useCallback(() => localStorage.removeItem('user'), [setUser]);
     const [stage, setStage] = useState('login');
 
-    const value = {login, logout, user, setUser, stage, setStage, stageStatus, setStageStatus};
+    const value = {logout, user, setUser, stage, setStage, stageStatus, setStageStatus, personal, setPersonal};
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 };
 
 export const PersistUser = () => {
-    const {user} = useUser();
+    const {user, personal} = useUser();
     useEffect(() => {
-        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('personal', JSON.stringify(personal));
     }, [user]);
 
     return <></>
