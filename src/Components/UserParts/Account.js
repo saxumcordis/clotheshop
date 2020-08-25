2d81064ed3e32fa2d90963d7b02590dff8d17405
saxumcordis <victor123952@gmail.com>
2020-08-25 17:13:55 +0300

    divide account component to smaller parts

--- src/Components/UserParts/Account.js
+++ src/Components/UserParts/Account.js
@@ -9,8 +9,86 @@ const setPersonalFormView = (id) => {
     form.hidden = !form.hidden;
 };
 
+const AccountEdit = () => {
+    const {personal, setPersonal} = useUser();
+    return (
+        <div className='account_right_personal_edit'>
+            <p className='account_right_personal_field'>
+                <span className='personal_field'>Имя:</span>
+                <span className='personal_field value'
+                      onClick={() => setPersonalFormView('personal_name')}>{personal.name}</span>
+            </p>
+            <div className="personal_edit_form" id="personal_name" hidden>
+                <p className="personal_edit_text">
+                    <label>Новое имя</label>
+                    <input required type="text" name="NAME" id="personal_name_input"/>
+                </p>
+                <button className="personal_button" onClick={() => {
+                    updatePersonalField('name', setPersonal)
+                }}>Сохранить
+                </button>
+            </div>
+            <p className='account_right_personal_field'>
+                <span className='personal_field'>Фамилия:</span>
+                <span className='personal_field value'
+                      onClick={() => setPersonalFormView('personal_surname')}>{personal.surname}</span>
+            </p>
+            <div className="personal_edit_form" id="personal_surname" hidden>
+                <p className="personal_edit_text">
+                    <label>Новая фамилия</label>
+                    <input required type="text" name="SURNAME" id="personal_surname_input"/>
+                </p>
+                <button className="personal_button" onClick={() => {
+                    updatePersonalField('surname', setPersonal)
+                }}>Сохранить
+                </button>
+            </div>
+            <p className='account_right_personal_field'>
+                <span className='personal_field'>Логин:</span>
+                <span className='personal_field'>{personal.email}</span>
+            </p>
+            <p className='account_right_personal_field'>
+                <span className='personal_field'>Пароль:</span>
+                <span className='personal_field value'
+                      onClick={() => setPersonalFormView('personal_password')}>********</span>
+            </p>
+            <div className="personal_edit_form" id="personal_password" hidden>
+                <p className="personal_edit_text">
+                    <label>Новый пароль</label>
+                    <span>Должен содержать от 7 до 15 символов, включать в себя буквы нижнего, верхнего регистров, цифру и спец.символ</span>
+                    <input required type="password" id="personal_password_input" name="PASSWORD"
+                           onChange={() => validatePassword('personal_password_input')}/>
+                </p>
+                <button className="personal_button" onClick={() => {
+                    if (validatePassword('personal_password_input')) updatePersonalField('password', setPersonal)
+                }}>Сохранить
+                </button>
+            </div>
+            <p className='account_right_personal_field'>
+                <span className='personal_field'>Дата рождения</span>
+                <span className='personal_field'>{personal.birth}</span>
+            </p>
+            <p className='account_right_personal_field'>
+                <span className='personal_field'>Телефон</span>
+                <span className='personal_field value'
+                      onClick={() => setPersonalFormView('personal_phone')}>{personal.phone}</span>
+            </p>
+            <div className="personal_edit_form" id="personal_phone" hidden>
+                <p className="personal_edit_text">
+                    <label>Новый телефон</label>
+                    <input required type="text" name="PHONE" id="personal_phone_input"/>
+                </p>
+                <button className="personal_button" onClick={() => {
+                    updatePersonalField('phone', setPersonal)
+                }}>Сохранить
+                </button>
+            </div>
+        </div>
+    )
+};
+
 const Account = () => {
-    const {user, logout, personal, setPersonal} = useUser();
+    const {user, logout} = useUser();
     const logOut = () => {
         logout();
         window.location.reload();
@@ -37,62 +115,7 @@ const Account = () => {
                         </div>
                         <div className='account_right_personal'>
                             <p>Подчёркнутые поля можно изменить. Для изменения поля кликните на него.</p>
-                            <div className='account_right_personal_edit'>
-                                <p className='account_right_personal_field'>
-                                    <span className='personal_field'>Имя:</span>
-                                    <span className='personal_field value'
-                                          onClick={() => setPersonalFormView('personal_name')}>{personal.name}</span>
-                                </p>
-                                <div className="personal_edit_form"id="personal_name" hidden>
-                                    <p className="personal_edit_text">
-                                        <label>Новое имя</label>
-                                        <input required type="text" name="NAME" id="personal_name_input"/>
-                                    </p>
-                                    <button className="personal_button" onClick={() => {updatePersonalField('name', setPersonal)}}>Сохранить</button>
-                                </div>
-                                <p className='account_right_personal_field'>
-                                    <span className='personal_field'>Фамилия:</span>
-                                    <span className='personal_field value' onClick={() => setPersonalFormView('personal_surname')}>{personal.surname}</span>
-                                </p>
-                                <div className="personal_edit_form" id="personal_surname" hidden>
-                                    <p className="personal_edit_text">
-                                        <label>Новая фамилия</label>
-                                        <input required type="text" name="SURNAME" id="personal_surname_input"/>
-                                    </p>
-                                    <button className="personal_button" onClick={() => {updatePersonalField('surname', setPersonal)}}>Сохранить</button>
-                                </div>
-                                <p className='account_right_personal_field'>
-                                    <span className='personal_field'>Логин:</span>
-                                    <span className='personal_field'>{personal.email}</span>
-                                </p>
-                                <p className='account_right_personal_field'>
-                                    <span className='personal_field'>Пароль:</span>
-                                    <span className='personal_field value' onClick={() => setPersonalFormView('personal_password')}>********</span>
-                                </p>
-                                <div className="personal_edit_form" id="personal_password" hidden>
-                                    <p className="personal_edit_text">
-                                        <label>Новый пароль</label>
-                                        <span>Должен содержать от 7 до 15 символов, включать в себя буквы нижнего, верхнего регистров, цифру и спец.символ</span>
-                                        <input required type="password" id="personal_password_input" name="PASSWORD" onChange={() => validatePassword('personal_password_input')}/>
-                                    </p>
-                                    <button className="personal_button" onClick={() => {if (validatePassword('personal_password_input')) updatePersonalField('password', setPersonal)}}>Сохранить</button>
-                                </div>
-                                <p className='account_right_personal_field'>
-                                    <span className='personal_field'>Дата рождения</span>
-                                    <span className='personal_field'>{personal.birth}</span>
-                                </p>
-                                <p className='account_right_personal_field'>
-                                    <span className='personal_field'>Телефон</span>
-                                    <span className='personal_field value' onClick={() => setPersonalFormView('personal_phone')}>{personal.phone}</span>
-                                </p>
-                                <div className="personal_edit_form" id="personal_phone" hidden>
-                                    <p className="personal_edit_text">
-                                        <label>Новый телефон</label>
-                                        <input required type="text" name="PHONE" id="personal_phone_input"/>
-                                    </p>
-                                    <button className="personal_button" onClick={() => {updatePersonalField('phone', setPersonal)}}>Сохранить</button>
-                                </div>
-                            </div>
+                            <AccountEdit/>
                         </div>
                     </div>
                 </div>
