c5a94a01b62ba7a4f8d7fbdca7c7a93163a8ccde
saxumcordis <victor123952@gmail.com>
2020-08-25 17:35:20 +0300

    add logout func

--- src/Service/Contexts/UserContext.js
+++ src/Service/Contexts/UserContext.js
@@ -9,7 +9,7 @@ export const UserProvider = ({children}) => {
     const [user, setUser] = useState(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : 'guest');
     const [personal, setPersonal] = useState(localStorage.getItem('personal') ? JSON.parse(localStorage.getItem('personal')) : []);
     const [stageStatus, setStageStatus] = useState(0);
-    const logout = useCallback(() => localStorage.removeItem('user'), [setUser]);
+    const logout = useCallback(() => {localStorage.removeItem('user'); localStorage.removeItem('personal')}, [setUser]);
     const [stage, setStage] = useState('login');
 
     const value = {logout, user, setUser, stage, setStage, stageStatus, setStageStatus, personal, setPersonal};
