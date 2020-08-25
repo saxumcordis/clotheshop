b911b0714ff8afb8dbd6437fb2bd14c5bb62fe17
saxumcordis <victor123952@gmail.com>
2020-08-25 18:27:24 +0300

    add changing address

--- src/Components/UserParts/Account/AddressEdit.js
+++ src/Components/UserParts/Account/AddressEdit.js
@@ -1,5 +1,6 @@
 import {useUser} from "../../../Service/Contexts/UserContext";
 import React from 'react';
+import {updateAddress} from "../../../Service/Server/personalSettings";
 
 
 export const AddressEdit = () => {
@@ -9,36 +10,37 @@ export const AddressEdit = () => {
         <div className="account_right_address_edit">
             <p className="address_field">
                 Страна
-                <input type="text" placeholder={personal.country}/>
+                <input type="text" id="address_country" placeholder={personal.country}/>
             </p>
             <p className="address_field">
                 Город
-                <input type="text" placeholder={personal.city}/>
+                <input type="text" id="address_city" placeholder={personal.city}/>
             </p>
             <p className="address_field">
                 Улица
-                <input type="text" placeholder={personal.street}/>
+                <input type="text" id="address_street" placeholder={personal.street}/>
             </p>
             <p className="address_field">
                 Индекс
-                <input type="text" placeholder={personal.zip}/>
+                <input type="text" id="address_zip" placeholder={personal.zip}/>
             </p>
             <p className="address_field">
                 Дом
-                <input type="text" placeholder={personal.building}/>
+                <input type="text" id="address_building" placeholder={personal.building}/>
             </p>
             <p className="address_field">
                 Корпус
-                <input type="text" placeholder={personal.pavilion}/>
+                <input type="text" id="address_pavilion" placeholder={personal.pavilion}/>
             </p>
             <p className="address_field">
                 Квартира/Офис
-                <input type="text" placeholder={personal.flat}/>
+                <input type="text" id="address_flat" placeholder={personal.flat}/>
             </p>
             <p className="address_field">
                 Телефон
-                <input type="text" placeholder={personal.phone}/>
+                <input type="text" id="address_phone" placeholder={personal.phone}/>
             </p>
+            <button className="personal_button" onClick={() => updateAddress(personal.address_id, setPersonal)}>Сохранить</button>
         </div>
     );
 };
--- src/Service/Server/personalSettings.js
+++ src/Service/Server/personalSettings.js
@@ -12,3 +12,35 @@ export const updatePersonalField = async function (field, update=null) {
 
     await fetch(url + data);
 };
+
+const updatePersonalAddress = (update, data) => {
+    const personal = JSON.parse(localStorage.getItem('personal'));
+    for (let field in data) {
+        if (personal.hasOwnProperty(field) && field !== 'id')
+            personal[field] = data[field];
+    }
+};
+
+export const updateAddress = async function (id, update) {
+    if (!window.confirm("Подтвердите изменение адреса")) {
+        return 0;
+    }
+    const data = {
+        id: id,
+        country: document.getElementById('address_country').value || document.getElementById('address_country').placeholder,
+        city: document.getElementById('address_city').value || document.getElementById('address_city').placeholder,
+        street: document.getElementById('address_street').value || document.getElementById('address_street').placeholder,
+        zip: document.getElementById('address_zip').value || document.getElementById('address_zip').placeholder,
+        building: document.getElementById('address_building').value || document.getElementById('address_building').placeholder,
+        pavilion: document.getElementById('address_pavilion').value || document.getElementById('address_pavilion').placeholder,
+        flat: document.getElementById('address_flat').value || document.getElementById('address_flat').placeholder,
+        phone: document.getElementById('address_phone').value || document.getElementById('address_phone').placeholder,
+    };
+    const token = JSON.parse(localStorage.getItem('user')).token;
+    const url = 'https://miktina.herokuapp.com/backend/user/account.php?address&new&token=';
+    const convertedData = url + token + "&address_id=" + data.id + "&country=" + data.country + "&city=" + data.city
+        + "&street=" + data.street + "&zip=" + data.zip + "&building=" + data.building + "&pavilion=" + data.pavilion
+        + "&flat=" + data.flat + "&phone=" + data.phone;
+    updatePersonalAddress(update, data);
+    await fetch(convertedData);
+};
