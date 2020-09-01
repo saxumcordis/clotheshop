4aba8cef3702cb8acbf08ffc0be7612577130b70
saxumcordis <victor123952@gmail.com>
2020-09-01 21:15:05 +0300

    init admin page

---
+++ src/Components/UserParts/Account/AdminPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useUser} from "../../../Service/Contexts/UserContext";
+
+const AdminLogin = () => {
+
+};
+
+export const AdminPage = () => {
+    const {user} = useUser();
+};
