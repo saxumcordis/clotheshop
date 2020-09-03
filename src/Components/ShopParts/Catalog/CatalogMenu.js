35e70ed7c1b1824aac50721df796e3b3817a3a1d
saxumcordis <victor123952@gmail.com>
2020-09-03 14:37:43 +0300

    add dynamic categories

--- src/Components/ShopParts/Catalog/CatalogMenu.js
+++ src/Components/ShopParts/Catalog/CatalogMenu.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from "react-router-dom";
 
 const CatalogMenu = () => {
+    const [categories, setCategories] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const categories = await fetch('https://miktina.herokuapp.com/backend/catalog/products.php?getCategories');
+            setCategories(await categories.json());
+
+        })();
+    }, [setCategories]);
+
     return (
         <div className="catalog_menu">
             <ul className="catalog_categories">
                 <li><Link to='/catalog'>КАТАЛОГ</Link></li>
-                <li className='catalog_category'><Link to='/catalog/category/1'>Костюмы длинные</Link></li>
-                <li className='catalog_category'><Link to='/catalog/category/2'>Костюмы короткие</Link></li>
+                {categories && categories.map(e => <li className='catalog_category'><Link to={'/catalog/category/' + e.category_id}>{e.category_name}</Link></li>)}
                 <li className='catalog_category'><Link to='/catalog/category/sale' style={{color: "red"}}>SALE</Link></li>
             </ul>
         </div>
