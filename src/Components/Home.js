4207cadb00ac1b3675da4d25a16c9f4eacd2dea3
saxumcordis <victor123952@gmail.com>
2020-09-05 18:05:43 +0300

    add css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,15 +7,15 @@ const Home = () => {
     return (
         <div className="with_footer">
             <div className="home">
+                <Link to='/catalog'>
                 <div className='main_picture'>
-                    <Link to='/catalog'><img
-                        src='https://iamstudio.ru/upload/iblock/6d0/6d01bbfb67c15689a193239f57dcfd39.jpg'/></Link>
                 </div>
+                </Link>
                 <div className='additional_pictures'>
                     <a href="https://instagram.com/miktina/"><img
                         src='https://iamstudio.ru/upload/iblock/82b/82b8598135960550d211242fc51059d2.jpg'/></a>
                     <Link to='/catalog/sale/'><img
-                        src='https://iamstudio.ru/upload/iblock/2a8/2a88112d1f0bf878818272cd00f632dd.jpg'/></Link>
+                        src='https://iamstudio.ru/upload/iblock/214/21451daef777285a22ff628df6dbdd26.jpg'/></Link>
                 </div>
                 <Footer/>
             </div>
