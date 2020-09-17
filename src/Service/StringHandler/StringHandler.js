19fb46cf64099bcb88e38cb7e51231df7b39f7c9
saxumcordis <victor123952@gmail.com>
2020-09-17 13:14:20 +0300

    add handlePrice

--- src/Service/StringHandler/StringHandler.js
+++ src/Service/StringHandler/StringHandler.js
@@ -3,5 +3,9 @@ import React from 'react';
 
 export const handleOrderDate = (date) => {
     const orderDate = date.split(' ')[0].split('-');
-    return orderDate[2] + " " + months[orderDate[1]] + " " + orderDate[0];
+    return orderDate[2] + " " + months[+orderDate[1]] + " " + orderDate[0];
+};
+
+export const handlePrice = (price) => {
+    return price.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ') + " P";
 };
