6ff77f14889e6aa5b2f0767fe0bc0212252c0a26
saxumcordis <victor123952@gmail.com>
2020-09-17 13:10:30 +0300

    add handleOrderDate

--- src/Service/StringHandler/StringHandler.js
+++ src/Service/StringHandler/StringHandler.js
@@ -1,3 +1,7 @@
-export const handleOrderDate = (date) => {
+import {months} from "../../Components/SystemParts/Months";
+import React from 'react';
 
-}
+export const handleOrderDate = (date) => {
+    const orderDate = date.split(' ')[0].split('-');
+    return orderDate[2] + " " + months[orderDate[1]] + " " + orderDate[0];
+};
