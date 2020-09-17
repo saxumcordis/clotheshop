1e153ae72664f754e2702faca6310b445075d4ff
saxumcordis <victor123952@gmail.com>
2020-09-17 13:07:38 +0300

    add months object

--- src/Components/UserParts/Account/AccountOrders.js
+++ src/Components/UserParts/Account/AccountOrders.js
@@ -2,21 +2,6 @@ import React, {useCallback, useEffect, useState} from 'react';
 import {useUser} from "../../../Service/Contexts/UserContext";
 import {useAsync} from "../../../Service/useAsync";
 
-const months = {
-    1: "Января",
-    2: "Февраля",
-    3: "Марта",
-    4: "Апреля",
-    5: "Мая",
-    6: "Июня",
-    7: "Июля",
-    8: "Августа",
-    9: "Сентября",
-    10: "Октября",
-    11: "Ноября",
-    12: "Декабря"
-};
-
 const Order = ({item}) => {
 
     console.log(item);
