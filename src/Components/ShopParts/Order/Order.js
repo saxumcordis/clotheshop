c5df886f5e566df0331adb5adcc10dcdf8e8ad01
saxumcordis <victor123952@gmail.com>
2020-09-19 17:07:58 +0300

    divide order component to small parts

--- src/Components/ShopParts/Order/Order.js
+++ src/Components/ShopParts/Order/Order.js
@@ -163,12 +163,48 @@ const Delivery = ({address}) => {
     </div>;
 };
 
-export const Order = () => {
 
-    const {personal, user} = useUser();
-    const {promo} = useCart();
+const Personal = () => {
+
+    const {personal} = useUser();
+    const {setOrderPersonal} = useOrder();
+    useEffect(() => setOrderPersonal(personal), [personal]);
+
+    return <div className="order_form">
+        {contactFields.map((e, index) => <p key={index} className="order_field">
+            <label>{e.title}</label>
+            <input required placeholder={personal[e.name]} type="text" name={e.name}
+                   id={"order_" + e.name + "_input"}/>
+        </p>)}
+    </div>
+};
+
+const Address = () => {
+    const {personal} = useUser();
     const [address, setAddress] = useState(address2);
     const [loading, setLoading] = useState(false);
+    console.log(address);
+
+    return <div className="order_form">
+        <p className="order_field">
+            <label>Адрес доставки</label>
+            <AddressSuggestions token="b58d963e5c648936410b2cb8d4db57f101d3c2a4"
+                //onChange={() => initAddress(user.token || "guest", document.getElementById("delivery_address_input").value, setAddress, setLoading)}
+                                inputProps={{
+                                    placeholder: handleAddress(personal) || "Укажите адрес доставки",
+                                    className: "order_field_address",
+                                    id: "delivery_address_input"
+                                }}
+                                suggestionClassName="address_suggestions"
+                                highlightClassName="address_suggestions_highlight"
+            />
+        </p>
+        {loading && "Вычисляем стоимость доставки"}
+        {address && !loading && <Delivery address={address}/>}
+    </div>
+}
+
+export const Order = () => {
 
     /*useEffect(() => {
         (async () => {
@@ -177,38 +213,15 @@ export const Order = () => {
             }
         })();
     }, [setAddress]);*/
-    console.log(address);
     return (
         <div className='with_footer'>
             <div className='global_giv'>
                 <div className="order_box">
                     <div className="order_left_column">
                         <div className="order_title"><h1>Контактная информация</h1><span>1</span></div>
-                        <div className="order_form">
-                            {contactFields.map((e, index) => <p key={index} className="order_field">
-                                <label>{e.title}</label>
-                                <input required placeholder={personal[e.name]} type="text" name={e.name}
-                                       id={"order_" + e.name + "_input"}/>
-                            </p>)}
-                        </div>
+                        <Personal/>
                         <div className="order_title"><h1>Доставка</h1><span>2</span></div>
-                        <div className="order_form">
-                            <p className="order_field">
-                                <label>Адрес доставки</label>
-                                <AddressSuggestions token="b58d963e5c648936410b2cb8d4db57f101d3c2a4"
-                                                    //onChange={() => initAddress(user.token || "guest", document.getElementById("delivery_address_input").value, setAddress, setLoading)}
-                                                    inputProps={{
-                                                        placeholder: handleAddress(personal) || "Укажите адрес доставки",
-                                                        className: "order_field_address",
-                                                        id: "delivery_address_input"
-                                                    }}
-                                                    suggestionClassName="address_suggestions"
-                                                    highlightClassName="address_suggestions_highlight"
-                                />
-                            </p>
-                            {loading && "Вычисляем стоимость доставки"}
-                            {address && !loading && <Delivery address={address}/>}
-                        </div>
+                        <Address/>
                     </div>
                 </div>
             </div>
